fb376cfc7012e9e7ded0d4410fc87b0561660740
Itamar Marom <itamarnine4@gmail.com>
2025-01-22 15:56:19 +0200

    Show alert if FS API is not supported

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router'
+
+const isFsApiSupported = (window as any).showOpenFilePicker !== undefined;
+
+onMounted(() => {
+  if (!isFsApiSupported) {
+    alert('Sorry, your browser does not support the File System Access API. Plese consider using another browser for installing this app.');
+  }
+});
 </script>
 
 <template>
